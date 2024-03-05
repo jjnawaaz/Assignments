fafe15ae99080069e3967da7af4b1b08e402f111
Junaid Nawaz <shaikhmohammedjunaidnawaz@gmail.com>
2024-03-05 16:02:51 +0530

    Removed anagram.js

--- anagram.js
+++
@@ -1,6 +0,0 @@
-function isAnagram(str1, str2) {
-    return str1.toLowerCase().split('').sort().join() === str2.toLowerCase().split('').sort().join()
-    
-   }
-   let a = isAnagram("sliete","listen")
-   console.log(a)
